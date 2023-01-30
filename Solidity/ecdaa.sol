c23a15e1cedaaea181d21f2c35be419305e57f84
rdubois-crypto <renaud.dubois@ledger.fr>
2023-01-30 16:27:50 +0100

    ECDAA implementation over BN254 (untested)

--- Solidity/ecdaa.sol
+++ Solidity/ecdaa.sol
@@ -26,11 +26,11 @@ contract ECDAA_ProofOfLedger {
     uint8 public constant _TAG_S8=8;
     uint8 public constant _MSG_S8=8;
     // Generator of G2
-    uint8 public constant P2_x_1 =11559732032986387107991004021392285783925812861821192530917403151452391805634;
-    uint8 public constant P2_x_0 =10857046999023057135944570762232829481370756359578518086990519993285655852781;
+    uint256 public constant P2_x_1 =11559732032986387107991004021392285783925812861821192530917403151452391805634;
+    uint256 public constant P2_x_0 =10857046999023057135944570762232829481370756359578518086990519993285655852781;
     
-    uint8 public constant P2_y_1 =4082367875863433681332203403145435568316851327593401208105741076214120093531;
-    uint8 public constant P2_y_0 =8495653923123431417604973247489272438418190587263600148770280649306958101930;
+    uint256 public constant P2_y_1 =4082367875863433681332203403145435568316851327593401208105741076214120093531;
+    uint256 public constant P2_y_0 =8495653923123431417604973247489272438418190587263600148770280649306958101930;
     
 
     //Gen1=E1([1,2]);
@@ -55,7 +55,12 @@ contract ECDAA_ProofOfLedger {
         uint256 y_re;
     }
     //TODO: initialize P2
-    EcPointG2 P2;
+    EcPointG2 public P2=EcPointG2(P2_x_1, P2_x_0, P2_y_1, P2_x_0);
+/*    P2.x_im=P2_x_1;
+    P2.x_re= P2_x_0;
+    P2.y_im= P2_y_1;
+    P2_y_re= P2_y_0;*/
+
     //Contract Storage
     EcPointG2 Pub_X;//Ledger Public key X part
     EcPointG2 Pub_Y;//Ledger Public Key Y part
@@ -85,44 +90,41 @@ contract ECDAA_ProofOfLedger {
 */
 
     // hash buffer to Finite Field
-    function HashZP(uint8[_BUFFER_S8] memory offset, uint32 buffer_S8) pure public returns(uint256 hZp) {
-     assembly{
-      
-      let h := keccak256(offset,buffer_S8 )
-      let hZp:= mod(h, modulus)
-     }
-      //TODO: recover result
-      return hZp;
+    function HashZP(bytes memory b) pure public returns(uint256 hZp) {
+
+     assembly {
+            hZp := keccak256(add(b, 0x20),mload(b))
+        }
+        hZp=hZp%modulus;
      }
 
     //call to ecAdd precompile
     function ecAdd(EcPointG1 memory P, EcPointG1 memory Q)   public returns(EcPointG1 memory res){
           uint256[4] memory input;
-
-           assembly {
            input[0]=P.x;
            input[1]=P.y;
            input[2]=Q.x;
            input[3]=Q.y;
            
+           assembly {
+          
               // TODO :how to call ecAdd precompile n06 ?
-           if iszero( if (call(not(0), 0x06, 0, input, 0x080, res, 0x40)))
+           if iszero( call(not(0), 0x06, 0, input, 0x080, res, 0x40))
            {
             revert(0, 0)
-           } 
+           } }
     
         
-        return res;
+       
     }
   
    // returns e(P1, P2)==e(Q1, Q2), which is equivalent to Precompile8(P1,P2, Q1, -Q2)
-   function ecPairingEquality(  EcPointG1 memory R1, EcPointG2 memory R2, EcPointG1 memory Q1, EcPointG2 memory Q2 ) pure public returns(bool  res)   {
+   function ecPairingEquality(  EcPointG1 memory R1, EcPointG2 memory R2, EcPointG1 memory Q1, EcPointG2 memory Q2 )  public returns(bool  res)   {
 
        //Q1=-Q1
        Q1.y=modulus-Q1.y;
        //bufer to paste 
-       uint8[_BUFFER_ECPAIRING_CMP] memory buffer;
-       uint256[12] memory input;
+      uint256[12] memory input;
        input[0]=R1.x;
        input[1]=R1.y;
        input[2]=R2.x_im;
@@ -142,14 +144,14 @@ contract ECDAA_ProofOfLedger {
         assembly {
               // TODO :how to call ecPairing precompile n08 ?
             if iszero(call(not(0), 0x08, 0, input, 0x0180, res, 0x20)) {
-            //    revert(0, 0)}
+                revert(0, 0)}
             }
 
         return res;
    }
 
     //call to ecSub precompile
-  function ecMul(uint256 s, EcPointG1 memory S)  pure public returns(EcPointG1 memory res){
+  function ecMul(uint256 s, EcPointG1 memory S)   public returns(EcPointG1 memory res){
       
        uint256[3] memory input;
 
@@ -159,15 +161,77 @@ contract ECDAA_ProofOfLedger {
        
        assembly {
             // call ecmul precompile
-            if iszero(call(not(0), 0x07, 0, input, 0x60, p, 0x40)) {
+            if iszero(call(not(0), 0x07, 0, input, 0x60, res, 0x40)) {
                 revert(0, 0)
             }
         }
         return res;
     }
 
-    function ECDAA_Verify(uint8[_TAG_S8] memory Tag, 
-                          uint8[_MSG_S8] memory Message, Signature memory S   ) external    {
+    EcPointG1 public debugP;
+
+    function test_ecAdd() public returns(EcPointG1 memory res)
+    {
+        EcPointG1 memory P1=EcPointG1(1,2);
+        EcPointG1 memory mP1=EcPointG1(1,modulus-2);
+      
+        EcPointG1 memory m2P=EcPointG1(1368015179489954701390400359078579693043519447331113978918064868415326638035, 
+        11970132820537103637166003141937572314130795164147247315533067598634108082819);
+
+        res=ecAdd(m2P,P1);
+        debugP=res;
+
+        assert(mP1.y==res.y);
+
+    }
+
+    uint256 public debug_hash;
+
+    function test_hashZp()  public returns (uint256 res)
+    {
+       
+     
+       // uint8[3] memory abc=[0x61, 0x62, 0x63];-> question for qdqd: why not working ?
+        bytes memory b = new bytes(3);
+        b[0]=0x61;b[1]=0x62;b[2]=0x63;
+
+       assembly {
+            res := keccak256(add(b, 0x20),mload(b))
+        }
+       
+       //expected hash of 'abc'  from https://gist.github.com/miguelmota/97b3a46291bc41a9b3054e75609d1dbf
+        assert(res==0x4e03657aea45a94fc7d47ba826c8d667c0d1e6e33a64a036ec44f58fa12d6c45);
+
+
+     debug_hash=res;
+    }
+
+    bytes public debug_buffer_out = new bytes(32);
+    
+    function test_ToBuff() public {
+        bytes memory debug_buffer = new bytes(32);
+  
+        debug_buffer[0]=0x61;debug_buffer[1]=0x62;debug_buffer[2]=0x63;
+
+        uint256 offset=0x20;
+        uint256 value=0x6061626364656667606162636465666760616263646566676061626364656667;
+       
+        //bytes memory x=abi.encodePacked(value);
+        assembly { mstore(add(debug_buffer, offset), value) }
+        //debug_buffer=abi.encodePacked(value);
+        bytes32 b = bytes32(value);
+
+        assert(b[0]!=0);
+        for(uint i=0;i<3;i++){
+            debug_buffer_out[i]=debug_buffer[i];
+        }
+    
+        assembly { mstore(add(debug_buffer, offset), value) }
+        
+    }
+
+    function ECDAA_Verify(uint256 Tag, 
+                          uint256 Message, Signature memory S   ) external    {
 
        // #3. H(n|H(S^s.W^-c|S|W|Data|H(KRD))) == c ? fail if not equal                    
      EcPointG1 memory t1=ecMul(S.s, S.S);
@@ -175,14 +239,54 @@ contract ECDAA_ProofOfLedger {
      EcPointG1 memory t2= ecMul(rmi_c, S.W);// U=s*S-i_c*W= s*S+(r-i_c)*W;
      EcPointG1 memory U=ecAdd(t1, t2);  
      //  #9. BigInteger c2=H(U|S|W|AdditionalData|h_KRD) 
-     uint8[_BUFFER_S8] memory buffer;
-     //TODO: paste everything in buffer
-     //TODO: return offset of buffer
-     uint256  c2=HashZP(buffer,_BUFFER_S8 );
+    bytes memory buffer=new bytes(_BUFFER_S8);
+    
+    uint256  offset=32;
+    uint256 temp=U.x;
+    assembly { mstore(add(buffer, offset),temp) } //Ux
+    offset=offset+32;
+
+    temp=U.x;
+    assembly { mstore(add(buffer, offset), temp) }//Uy
+    offset=offset+32;
+
+    temp=S.S.x;
+    assembly { mstore(add(buffer, offset),temp) } //Sx
+    offset=offset+32;
+
+    temp=S.S.y;
+    assembly { mstore(add(buffer, offset),temp) } //Sy
+    offset=offset+32;
+
+    temp=S.W.x;
+    assembly { mstore(add(buffer, offset),temp) } //Sx
+    offset=offset+32;
+
+    temp=S.W.y;
+    assembly { mstore(add(buffer, offset),temp) } //Sy
+    offset=offset+32;
+
+    temp=S.W.y;
+    assembly { mstore(add(buffer, offset),temp) } //Sy
+    offset=offset+32;
+
+    assembly { mstore(add(buffer, offset),Tag) } //Tag
+    assembly { mstore(add(buffer, offset),Message) } //Msg
+
+    uint256  c2=HashZP(buffer );
     //#11 c=H(n|c2)
-    //TODO: paste everything in buffer
-     uint256  c=HashZP(buffer, 2*_ORDER_S8 );
-     assert(c==S.i_c);
+ //   bytes memory buffer2=new bytes(_BUFFER_S8);, would cost stack size
+ // to spare stack size we reuse buffer and modify its length
+    temp=2*_MODULUS_S8;
+    assembly { mstore(buffer,temp) }
+    offset=32;
+    temp=S.n;
+
+    assembly { mstore(add(buffer, offset),temp) } //n
+    assembly { mstore(add(buffer, offset),c2) } //n
+    
+    uint256  c=HashZP(buffer );
+    assert(c==S.i_c);
     //#4 e(R,Y)!=e(S,P2), fail if not equal
     bool res=ecPairingEquality(S.R, Pub_Y, S.S, Pub_X);
     //#5 e(T,P2)==e(R.W, X) ? fail if not equal  
